6978fd5e21ede7ecf0e610d53a0bda6b8112ce0e
Lori Corbani <lori.corbani@jax.org>
2017-08-04 12:13:14 -0400

    TR12250/Lit Triage

--- pdfGetFullText.sh
+++ pdfGetFullText.sh
@@ -30,7 +30,8 @@ if [ -f ${FILE} ]; then
 	rm ${FILE}
 fi
 
-pdftotext -q -nopgbrk $1 $FILE
+#pdftotext -q -nopgbrk $1 $FILE
+pdftotext -enc ASCII7 -q -nopgbrk $1 $FILE
 EXITCODE=$?
 cat ${FILE}
 rm ${FILE}
