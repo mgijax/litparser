93f4396bb72c2fbd14e457af83e59a3020052d7c
Lori Corbani <lori.corbani@jax.org>
2020-04-03 13:12:16 -0400

    TR13204/Infrastructure/python 3

--- getDOI.py
+++ getDOI.py
@@ -1,11 +1,8 @@
-#!/usr/local/bin/python
-
 # Name: getFirstDOI.py
 # Purpose: extract the first DOI ID for the specified paper and write it to stdout
 
 import sys
 sys.path.insert(0, '/usr/local/mgi/live/lib/python')
-sys.path.insert(0, '/home/jsb/jax/prod/lib/python')
 
 import os
 import PdfParser
